5bd6cf3efc4810d4ab8d12b2022d2b9f3942c53e
Matt Green <mgreen9@gmail.com>
2014-04-23 20:18:06 -0700

    changed mongo url to use env var

--- config/config.js
+++ config/config.js
@@ -59,7 +59,7 @@ module.exports = {
     // }
   },
   production: {
-    db: 'mongodb://vibe:ksdiocskla@ds033487.mongolab.com:33487/heroku_app24476177/vibe',
+    db: process.env.MONGOLAB_URI,
     root: rootPath,
     app: {
       name: 'Vibe'
