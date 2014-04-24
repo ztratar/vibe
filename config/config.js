85d48a877b5a58bab239ed0f4f7d398edae968b8
Matt Green <mgreen9@gmail.com>
2014-04-23 20:45:16 -0700

    minor config stuff

--- config/config.js
+++ config/config.js
@@ -59,7 +59,7 @@ module.exports = {
     // }
   },
   production: {
-    db: process.env.MONGOLAB_URI,
+    db:  process.env.MONGOLAB_URI || 'mongodb://vibe:ksdiocskla@ds033487.mongolab.com:33487/heroku_app24476177/vibe',
     root: rootPath,
     app: {
       name: 'Vibe'
