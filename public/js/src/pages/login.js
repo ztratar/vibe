dde606c800b0f38fc70bcd104781db697aab1664
Zachary Tratar <ztratar@Zacharys-MacBook-Air.local>
2014-07-11 19:28:48 -0700

    Log in page tap enabled with morphing

--- public/js/src/pages/login.js
+++ public/js/src/pages/login.js
@@ -1,5 +1,8 @@
 import 'jquery';
 
+module Hammer from 'hammer';
+module jqueryHammer from 'jqueryHammer';
+
 $(function() {
 	if (window.pageName !== 'login') {
 		return;
@@ -9,9 +12,15 @@ $(function() {
 	$('form.login').on('submit', function() {
 		var email = $(this).find('input[name="email"]').val(),
 			password = $(this).find('input[name="password"]').val(),
-			$error = $(this).find('.alert-danger');
+			$error = $(this).find('.alert-danger'),
+			$button = $(this).find('button');
 
 		$error.html('').hide();
+
+		$button.addClass('hit');
+		setTimeout(function() {
+			$button.removeClass('hit');
+		}, 400);
 
 		if (!email.length) {
 			$error.html('Please enter an email').show();
@@ -23,6 +32,9 @@ $(function() {
 			return false;
 		}
 
+		$button.attr('disabled', 'disabled');
+		$button.html('Logging in...');
+
 		$.ajax({
 			type: 'POST',
 			url: '/api/login',
@@ -32,6 +44,8 @@ $(function() {
 			},
 			success: function(d) {
 				if (d.error) {
+					$button.attr('disabled', false);
+					$button.html('Log In');
 					$error.html(d.error).show();
 					return false;
 				}
