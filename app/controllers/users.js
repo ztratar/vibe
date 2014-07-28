b38d0ba068df47fbf0f85d84b5bbadcc31991b87
Zachary Tratar <ztratar@Zacharys-MacBook-Air-2.local>
2014-07-27 23:55:54 -0700

    Attempting to debug Zains user update error

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -460,8 +460,9 @@ exports.update = function(req, res, next){
 
 		var changedAvatar = false;
 
+		// Doesn't cast when admin is overriding requests user
 		if (user.company instanceof Company) {
-			user.company = user.company._id;
+			user.setValue('company', user.company._id);
 			console.log('was company. Now is', user.company);
 		}
 
