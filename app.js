fc97315f89dc6bb36d1e6851cedc7f016bc793d6
Zachary Tratar <ztratar@Zacharys-MacBook-Air-2.local>
2014-07-28 13:04:49 -0700

    Enable keep alive

--- app.js
+++ app.js
@@ -18,51 +18,62 @@ var env = process.env.NODE_ENV || 'development',
 // Bootstrap db connection
 console.log('-----');
 console.log('Attempting to create Mongoose connection...');
-var mongooseConnection = mongoose.connect(config.mongoosedb, function(e) {
-	console.log('Mongoose connection created!');
-	console.log('-----');
+var mongooseConnection = mongoose.connect(config.mongoosedb, {
+		server: {
+			socketOptions: {
+				keepAlive: 1
+			}
+		},
+		replset: {
+			socketOptions: {
+				keepAlive: 1
+			}
+		}
+	}, function(e) {
+		console.log('Mongoose connection created!');
+		console.log('-----');
 
-	// Bootstrap models
-	var models_path = __dirname + '/app/models',
-		models = [
-			'accessRequest',
-			'answer',
-			'chat',
-			'company',
-			'feedback',
-			'metaquestion',
-			'notification',
-			'questionInstance',
-			'question',
-			'post',
-			'user',
-			'userInvite'
-		];
+		// Bootstrap models
+		var models_path = __dirname + '/app/models',
+			models = [
+				'accessRequest',
+				'answer',
+				'chat',
+				'company',
+				'feedback',
+				'metaquestion',
+				'notification',
+				'questionInstance',
+				'question',
+				'post',
+				'user',
+				'userInvite'
+			];
 
-	_.each(models, function(modelName) {
-		require(models_path + '/' + modelName);
+		_.each(models, function(modelName) {
+			require(models_path + '/' + modelName);
+		});
+
+		// bootstrap passport config
+		require('./config/passport')(passport, config);
+
+		console.log('Setting up express & http...');
+		app = express();
+		var server = http.createServer(app);
+
+		// express settings
+		require('./config/express')(app, config, passport, mongooseConnection);
+
+		// Bootstrap routes
+		require('./config/routes')(app, passport);
+
+		// Start faye
+		require('./app/live')(server);
+
+		// Start the app by listening on <port>
+		server.listen(port);
+		console.log('Express app started on port ' + port);
 	});
-
-	// bootstrap passport config
-	require('./config/passport')(passport, config);
-
-	console.log('Setting up express & http...');
-	app = express();
-	var server = http.createServer(app);
-
-	// express settings
-	require('./config/express')(app, config, passport, mongooseConnection);
-
-	// Bootstrap routes
-	require('./config/routes')(app, passport);
-
-	// Start faye
-	require('./app/live')(server);
-
-	// Start the app by listening on <port>
-	server.listen(port);
-	console.log('Express app started on port ' + port);
-});
 
 // expose app
 exports = module.exports = app;
